fb6a8f4f56884219e38d9a5b513e60bc39e8a941
al3mat <tyapa94@gmail.com>
2018-04-09 13:00:36 +0200

    Correzioni playlist. Aggiunta del solo nome file.

--- src/sample/listTitle.java
+++ src/sample/listTitle.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,11 +14,18 @@ public class listTitle
 {
     ListView lv = new ListView();
     ObservableList<String> st;
+    List<String> nameList = new ArrayList<>();
 
     void takeTitles(List<String> names)
     {
-        st = FXCollections.observableArrayList(names);
+        File f;
 
+        for (int i = 0; i < names.size(); i++){
+            f = new File(names.get(i));
+            nameList.add(i, f.getName());
+        }
+
+        st = FXCollections.observableArrayList(nameList);
         lv.setItems(st);
     }
 }
